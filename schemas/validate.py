67c280f62b65b5b259517b15174fe2f55bc5996b
Kells Kearney <keruzu@gmail.com>
2022-01-17 17:03:31 -0500

    Clean up validator

--- schemas/validate.py
+++ schemas/validate.py
@@ -1,19 +1,48 @@
 #!/usr/bin/env python
 
+import sys
+from argparse import ArgumentParser
 import json
 from jsonschema import validate
 import yaml
 
 
-schema_file = "trapmux.json"
-config_file = "../tools/trapmux_min.yml"
+class Validator(object):
 
-fd = open(schema_file)
-schema = json.load(fd)
-print(json.dumps(schema, indent=2))
+    def run(self):
+        self.buildParser()
+        self.buildOptions()
+        self.parseOptions()
 
-fd = open(config_file)
-data = yaml.safe_load(fd)
+        fd = open(self.args.schema)
+        schema = json.load(fd)
 
-validate(instance=data, schema=schema)
+        if self.args.fmt:
+            print(json.dumps(schema, indent=2))
+            sys.exit(0)
+
+        fd = open(self.args.config)
+        config = yaml.safe_load(fd)
+
+        validate(instance=config, schema=schema)
+
+    def buildParser(self):
+        self.parser = ArgumentParser(description='Validate YAML file against JSON-schema defintion')
+
+    def buildOptions(self):
+        self.parser.add_argument('--schema', default="trapmux.json",
+                    help='JSON schema file')
+        self.parser.add_argument('--config', default="../tools/trapmux_min.yml",
+                    help='YAML configuration file')
+
+        self.parser.add_argument('--fmt', default=False, action='store_true',
+                    help='Format schema file and exit')
+
+    def parseOptions(self):
+        self.args = self.parser.parse_args()
+
+
+if __name__ == '__main__':
+    cmd = Validator()
+    cmd.run()
 
